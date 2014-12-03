c499ebc6b534007523632e60f8f2a8be162dab30
adampru209 <adam.proulx209@gmail.com>
2014-12-03 09:50:43 -0800

    12.3.2014
    Added a templates folder to hold the views for different states. Added a
    simple state transition from main menu to timeline to self assessment.

--- prototype/www/js/app.js
+++ prototype/www/js/app.js
@@ -3,17 +3,48 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+angular.module('prototype', ['ionic'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
-  });
+.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+
+  .state('mainMenu', {
+    url: '/MainMenu',
+    templateUrl: 'templates/MainMenu.html',
+    controller: 'MainCtrl'
+  })
+  
+  .state('timeline', {
+    url: '/Timeline',
+    templateUrl: 'templates/Timeline.html',
+    controller: 'MainCtrl'  
+  })
+
+  .state('selfAssess', {
+    url: '/selfAssess',
+    templateUrl: 'templates/SelfAssessment.html',
+    controller: 'MainCtrl'
+  })
+
+  $urlRouterProvider.otherwise("/MainMenu");
+})
+
+
+.controller('MainCtrl', function($scope, $state) {
+  $scope.toTimeline = function(){
+    $state.go('timeline');
+  }
+
+  $scope.toSelfAssess = function(){
+    $state.go('selfAssess');
+  }
+
+  /*$scope.menu = [
+    { title: 'Timeline'},
+    { title: 'Career Binder'},
+    { title: 'The Serious Job Seeker'},
+    { title: 'Success Stories'},
+    { title: 'Why It Works!'},
+    { title: 'About Us'}
+  ];*/
 })
