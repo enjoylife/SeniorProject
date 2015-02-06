3982ee37e85086e4270c4f9b9feebb25f4262129
davidsaechao <dks95961@gmail.com>
2015-02-06 13:57:51 -0800

    DatePicker plugin
    Removed unused actionSheet

--- prototype1/www/js/calendarCtrls.js
+++ prototype1/www/js/calendarCtrls.js
@@ -10,14 +10,26 @@ app.controller('CalendarCtrl', function ($scope, $cordovaCalendar) {
 		calendarColor: '#FF0000'
 	  }).then(function (result) {
 		// success
+		console.log(result);
 	  }, function (err) {
 		// error
 	  });
 	  
-	  alert(windows.plugins);
+	  $cordovaCalendar.createEventInteractively({
+		title: 'Space Race',
+		location: 'The Moon',
+		notes: 'Bring sandwiches',
+		startDate: new Date(2015, 0, 6, 18, 30, 0, 0, 0),
+		endDate: new Date(2015, 1, 6, 12, 0, 0, 0, 0)
+	  }).then(function (result) {
+		// success
+	  }, function (err) {
+		// error
+	  });
+	  
 	  
 
-  }
+  };
   
   
 
