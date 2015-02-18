a735a810be926e96b95f3d09940a064d5e8f0707
matthew clemens <matt.d.clemens@gmail.com>
2015-02-17 18:04:29 -0800

    side nav view and non abstract content templates

---
+++ www/js/contentOutline.js
@@ -0,0 +1,29 @@
+// TODO Replace the subsection's section order with meaninful data
+var contentOutline = {
+	ordering: ["intro","assement", "search", "resume","interview"],
+
+	"intro":{
+		leadIn:"Test paragraph which gives user an idea of whats in the section",
+		sectionOrder :['intro','intro','intro','intro']
+	},
+
+	"assement": {
+		leadIn:"Test paragraph which gives user an idea of whats in the section",
+		sectionOrder :['section1','section2','section3','section4']
+	},
+
+	"search": {
+		leadIn:"Test paragraph which gives user an idea of whats in the section",
+		sectionOrder :['section1','section2','section3','section4']
+	},
+
+	"resume" : {
+		leadIn:"Test paragraph which gives user an idea of whats in the section",
+		sectionOrder :['section1','section2','section3','section4']
+},
+
+	"interview" : {
+		leadIn:"Test paragraph which gives user an idea of whats in the section",
+		sectionOrder :['section1','section2','section3','section4']
+	}
+}
