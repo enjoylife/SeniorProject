7b1e5942680e1cdb60100d4e8079af9745d3b82b
matthew clemens <matt.d.clemens@gmail.com>
2015-02-21 15:11:06 -0800

    profile css and initial camera integration

--- www/js/profileCtrl.js
+++ www/js/profileCtrl.js
@@ -1,3 +1,58 @@
-app.controller('profile',[function(){
-  
-}])
+app.controller('profile',function($scope){
+  var quoteArray = [{quote:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.", cite:"Someone famous"}];
+  $scope.quoteObj = quoteArray[0];
+  $scope.picture = localStorage.getItem('userProfilePicture');
+
+  $scope.randQuote = function(){
+    var i = _.random(0, quoteArray.length -1);
+    $scope.quoteObj = quoteArray[i];
+  }
+
+
+  function onSuccess(imageURI) {
+    var image = document.getElementById('profileImage');
+    image.src = imageURI;
+  }
+
+  function onFail(message) {
+      alert('Failed because: ' + message);
+  }
+
+  $scope.getPhoto = function() {
+    Camera.getPicture().then(function(imageURI) {
+      console.log(imageURI);
+      localStorage.setItem('userProfilePicture',imageURI )
+    }, function(err) {
+      console.err(err);
+    },
+    {
+      sourceType:Camera.PictureSourceType.PHOTOLIBRARY,
+      quality: 50,
+      destinationType: Camera.DestinationType.FILE_URI,
+      targetWidth: 320,
+      targetHeight: 320,
+      saveToPhotoAlbum: false
+    });
+  }
+
+$scope.randQuote()
+
+})
+
+app.factory('Camera', ['$q', function($q) {
+
+  return {
+    getPicture: function(options) {
+      var q = $q.defer();
+
+      navigator.camera.getPicture(function(result) {
+        // Do any magic you need
+        q.resolve(result);
+      }, function(err) {
+        q.reject(err);
+      }, options);
+
+      return q.promise;
+    }
+  }
+}]);
