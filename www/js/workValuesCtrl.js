78497a688b09268a08a6b1d88cfc5d08fbd114fd
adampru209 <adam.proulx209@gmail.com>
2015-04-08 18:24:29 -0700

    Completed Work Values Assessment
    -Added all options and values to work values asmnt

--- www/js/workValuesCtrl.js
+++ www/js/workValuesCtrl.js
@@ -18,6 +18,96 @@ app.controller('workValuesCtrl', ['$scope', '$localstorage', 'workValueService',
 			body:'A job that allows me to perform many different tasks.',
 			value:null
 		},
+		{
+			head:'Flexibility: ',
+			body:'A job that allows me to set my own schedule and change my working style as I wish.',
+			value:null
+		},
+		{
+			head:'Learning: ',
+			body:'A job in which I can learn and enhance my skills and abilities.',
+			value:null
+		},
+		{
+			head:'Intensity: ',
+			body:'A job that requires much time and attention, sustained effort and intense concentration.',
+			value:null
+		},
+		{
+			head:'Consistency: ',
+			body:'A job in which my duties are clearly defined and my responsibilities are stable and predictable.',
+			value:null
+		},
+		{
+			head:'Independence: ',
+			body:'A job in which I can do things my own way and make my own decisions.',
+			value:null
+		},
+		{
+			head:'Teamwork: ',
+			body:'A job in which I can work closely with a number of other individuals.',
+			value:null
+		},
+		{
+			head:'Leadership: ',
+			body:'A job in which I can lead, supervise and influence others.',
+			value:null
+		},
+		{
+			head:'Job Security: ',
+			body:'A job that I know I will be able to keep as long as I want.',
+			value:null
+		},
+		{
+			head:'Stability: ',
+			body:'A job with a solid, stable, established and predictable organization.',
+			value:null
+		},
+		{
+			head:'Financial Security: ',
+			body:'A job that provides adequately for my needs, allowing me to live comfortably.',
+			value:null
+		},
+		{
+			head:'High Paying: ',
+			body:'A job in which I can earn a good deal of money.',
+			value:null
+		},
+		{
+			head:'Financial Growth: ',
+			body:'A job that allows for continuing salary growth and opportunity.',
+			value:null
+		},
+		{
+			head:'Career Growth: ',
+			body:'A job that promises advancement, opportunity and increasing responsibility.',
+			value:null
+		},
+		{
+			head:'Prestige: ',
+			body:'A job that is important and for which others might look up to me.',
+			value:null
+		},
+		{
+			head:'Support: ',
+			body:'A job working for someone I admire and respect and to whose success I can contribute.',
+			value:null
+		},
+		{
+			head:'Respect: ',
+			body:'A job in which my work is recognized and applauded and where I feel valued as a person.',
+			value:null
+		},
+		{
+			head:'Public Service: ',
+			body:'A job in which allows me to make the world a better or safer place to live.',
+			value:null
+		},
+		{
+			head:'Helping People: ',
+			body:'A job in which I can help people improve their lives.',
+			value:null
+		}
 	]
 
 
