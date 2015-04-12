5f98226337610bc67eeb628d7cf7688e121fbe6f
matthew clemens <matt.d.clemens@gmail.com>
2015-04-12 13:39:45 -0700

    update complexity report

---
+++ report/files/www_js_services_js/report.history.js
@@ -0,0 +1,1 @@
+__history = [{"date":"Sun, 12 Apr 2015 20:35:43 GMT","sloc":202,"lloc":99,"functions":33,"deliveredBugs":1.320381483360704,"maintainability":78.59423062960069,"lintErrors":26,"difficulty":25.3125}]
